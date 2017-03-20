1cb9e96740f38fba484f0d6165c445978a623f6c
刘宇 <liu.yu@puscene.com>
2017-03-20 16:38:30 +0800

    add page plug

--- src/main/java/mw/pos/controllers/AddrController.java
+++ src/main/java/mw/pos/controllers/AddrController.java
@@ -1,12 +1,20 @@
 package mw.pos.controllers;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+ 
 import com.pos.entity.*;
 
 import mw.pos.handler.ResultInfo;
@@ -18,6 +26,7 @@ import mw.pos.util.ConfigUtil;
 public class AddrController {
 	@Resource
 	private IAddrService addrService = null;
+	private static Logger logger = Logger.getLogger(AddrController.class);
 
 	@RequestMapping(value = "insert", method = RequestMethod.POST)
 	public ResultInfo<Integer> insert(@RequestBody Addr tbaddr) {
@@ -46,6 +55,19 @@ public class AddrController {
 	public ResultInfo<Addr> selectByPrimaryKey(AddrKey tbaddrKey) {
 		ResultInfo<Addr> resultInfo = new ResultInfo<Addr>();
 		resultInfo.setData(addrService.selectByPrimaryKey(tbaddrKey));
+		resultInfo.setStatus(true);
+		resultInfo.setMessage(ConfigUtil.getSuccess());
+		return resultInfo;
+	}
+
+	@RequestMapping(value = "getList", method = RequestMethod.GET)
+	@ResponseBody
+	public ResultInfo<PageInfo<Addr>> getList(Addr tbaddr) {
+		logger.info("logger=" + JSON.toJSONString(tbaddr));
+		PageHelper.startPage(1,10);
+		PageInfo<Addr> pageInfo = new PageInfo<Addr>(addrService.getList(tbaddr));
+		ResultInfo<PageInfo<Addr>> resultInfo = new ResultInfo<PageInfo<Addr>>();
+		resultInfo.setData(pageInfo);
 		resultInfo.setStatus(true);
 		resultInfo.setMessage(ConfigUtil.getSuccess());
 		return resultInfo;
